c875bbfa4fa9819da49777354d7e1ad5988846e6
zhangtengccie <zhangtengccie@126.com>
2021-06-29 22:35:09 +0800

    Update conn.py

--- conn.py
+++ conn.py
@@ -10,9 +10,11 @@ str1 = 'TCP server 172.16.1.101:443 localserver 172.16.66.1:53710, idle 0:01:09,
 re_match = re.match(
     '([A-Z]{3})\s+[a-z]{6}\s+(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}:\d{1,5})\s+\w{11}\s+(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}:\d{1,5}),\s+\w{4}\s+([0-2]:[0-2][0-3]:[0-5][0-9]),\s+\w{5}\s+(\d{8}),\s+\w{5}\s+([A-Z]{3})',
     str1).groups()
+idel_time = re_match[3].split(':')
+
 print(f'{"protcol":15}:' + re_match[0])
 print(f'{"server":15}:' + re_match[1])
 print(f'{"localserver":15}:' + re_match[2])
-print(f'{"idle":15}:' + re_match[3])
+print(f'{"idle":15}:' + idel_time[0]+'时'+idel_time[1]+'分'+idel_time[2]+'秒')
 print(f'{"bytes":15}:' + re_match[4])
 print(f'{"flags":15}:' + re_match[5])
